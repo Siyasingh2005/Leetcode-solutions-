f0c6a173050afd61caaed19a2c825f6c7524a38f
Siyasingh2005 <siyasingh2005@gmail.com>
2025-05-05 15:31:55 +0530

    234- Plindrome linked list.cpp

---
+++ 234- Plindrome linked list.cpp
@@ -0,0 +1,22 @@
+// Problem : leetcode.com/problems/palindrome-linked-list/description/
+// Language : C++
+// Author : Siya Singh 
+
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+      stack<int> stack;
+      ListNode* curr = head;
+      while( curr) {
+        stack.push(curr->val);
+        curr = curr-> next;
+
+      }
+      curr = head; 
+      while(curr && curr->val == stack.top()) {
+        stack.pop();
+        curr = curr-> next;
+      }
+      return curr == nullptr;
+    }
+};
