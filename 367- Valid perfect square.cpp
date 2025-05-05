77b9061fdefa1f5c1a3d1057c435bdc05a5db6b0
Siyasingh2005 <siyasingh2005@gmail.com>
2025-05-05 15:33:18 +0530

    367- Valid perfect square.cpp

---
+++ 367- Valid perfect square.cpp
@@ -0,0 +1,16 @@
+// Problem: https://leetcode.com/problems/valid-perfect-square/description/
+// Language : C++
+// Author : Siya Singh
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+      for (long i = 1; i * i <= num; i++) {
+        if (i * i == num) {
+            return true;
+        }
+    }
+    return false;
+
+    }
+};
